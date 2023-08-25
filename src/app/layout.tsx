c93efb2a06f622295117cb40935689d84c5a7f8b
cocc2021 <cc9782564@gmail.com>
2023-08-24 20:51:06 -0400

    base

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Flexxible App",
+  description: "Showcase and discover remarable developer projects",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+		  <body>
+        <Navbar />
+        <main>
+					{children}
+				</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
